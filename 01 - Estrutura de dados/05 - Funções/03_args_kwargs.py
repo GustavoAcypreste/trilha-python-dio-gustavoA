085730aa23f7217eee13324a81fb2bc9f795cf77
GustavoAcypreste <gustavoacyprestemr@gmail.com>
2023-09-27 21:41:00 -0300

    alterações até a aula 1 de funções

--- 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
+++ 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
@@ -6,6 +6,7 @@ def exibir_poema(data_extenso, *args, **kwargs):
 
 
 exibir_poema(
+    "quarta, 27 de setembro de 2023",
     "Zen of Python",
     "Beautiful is better than ugly.",
     "Explicit is better than implicit.",
